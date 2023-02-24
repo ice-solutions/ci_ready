919e8f5c970fc17d7c4603b6dc8713848746146c
ian <ian.escarro@gmail.com>
2023-02-24 10:00:37 +0800

    more date functions

--- application/helpers/date_helper.php
+++ application/helpers/date_helper.php
@@ -1,7 +1,7 @@
 <?php
 
-define('DEFAULT_DATETIME_FORMAT', 'Y-m-d H:i:s');
 define('DEFAULT_DATE_FORMAT', 'Y-m-d');
+define('DEFAULT_DATETIME_FORMAT', 'Y-m-d H:i:s');
 define('READABLE_DATETIME_FORMAT', 'M,d Y g:i A');
 
 function now($format = DEFAULT_DATETIME_FORMAT) {
@@ -9,8 +9,15 @@ function now($format = DEFAULT_DATETIME_FORMAT) {
   return $date->format($format);
 }
 
-function yesterday() {
-  return date(DEFAULT_DATETIME_FORMAT, strtotime('-1 days', strtotime(now())));
+function yesterday($format = DEFAULT_DATETIME_FORMAT) {
+  return date($format, strtotime('-1 days', strtotime(now())));
+}
+
+function this_month() {
+  $start = date('Y-m', strtotime(now())) . '-01 00:01';
+  $next_month = date('Y-m-d', strtotime('+1 month', strtotime($start))) . ' 23:59';
+  $end = date('Y-m-d', strtotime('-1 day', strtotime($next_month))) . ' 23:59';
+  return array($start, $end);
 }
 
 function month() {
@@ -21,4 +28,8 @@ function month() {
 function year() {
   $year = date('Y', strtotime(now()));
   return $year;
+}
+
+function today($format = DEFAULT_DATETIME_FORMAT) {
+  return array(now($format), now($format));
 }
