0de40908745c220a1d2ff1289ef1d8db7e1f1809
ian <ian.escarro@gmail.com>
2024-03-24 10:23:54 +0800

    register validate

--- application/helpers/user_helper.php
+++ application/helpers/user_helper.php
@@ -21,6 +21,13 @@ function register_form() {
   return $user;
 }
 
+function register_form_validate() {
+  $obj = &get_instance();
+  $obj->form_validation->set_rules('email', 'Email', 'required');
+  $obj->form_validation->set_rules('password', 'Password', 'required');
+  $obj->form_validation->set_rules('confirm_password', 'Confirm Password', 'required|matches[password]');
+}
+
 function profile_form() {
   $obj = &get_instance();
   $user = array(
